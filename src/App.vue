163a691e4eed0b1f7baf3ae4654b7a7d7af18f36
Murray O'Brien <61970823+murrayobrienIT@users.noreply.github.com>
2022-04-07 20:51:15 +0200

    comit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
+<script>
+import HomeView from "./views/HomeView.vue";
+export default {
+  components: {
+    HomeView,
+  },
+};
+</script>
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <HomeView />
+    <!-- <router-view /> -->
   </div>
 </template>
 
